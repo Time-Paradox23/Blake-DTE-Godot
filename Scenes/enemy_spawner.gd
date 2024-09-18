78013a85a4799444312fe6d61055dc3940a8ed79
Time-Paradox23 <blabro36@kingshigh.school.nz>
2024-09-18 13:25:25 +1200

    Version 1.1.6
    Added hurt animation to player

--- Scenes/enemy_spawner.gd
+++ Scenes/enemy_spawner.gd
@@ -10,7 +10,8 @@ const ZOMBIE = preload("res://Scenes/zombie.tscn")
 func spawn_enemy():
 	var enemy = ZOMBIE.instantiate()
 	var spawn_point = spawn_points.pick_random
-
+	enemy.global_position = spawn_point.global_position
+	world.add_child(enemy)
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_enemy()
